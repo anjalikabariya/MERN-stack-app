7d542009ba492532029eb0c581bcdb88dd6da7ba
anjalikabariya <anjali.kabariya21@gmail.com>
2023-09-01 12:08:56 +0400

    added frontend authentication - login/register/logout functionality

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from '../features/auth/authSlice';
 
 export const store = configureStore({
   reducer: {
-    
+    auth: authSlice,
   },
 });
+
+export default store;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from "react";
 import { FaUser } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -10,15 +15,50 @@ function Register() {
   });
 
   const { name, email, password, confirmPassword } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    if (isSuccess || user) {
+      navigate("/");
+    }
+    dispatch(reset());
+  }, [isError, message, isSuccess, isLoading, navigate, dispatch, user]);
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
     }));
   };
+
   const onSubmit = (e) => {
     e.preventDefault();
+
+    if (password !== confirmPassword) {
+      toast.error("passwords do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      dispatch(register(userData));
+    }
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <>
       <section className="heading">
